25bbc2bdce632c0094eb09f440ae0bce2b68af71
Victor Yohanna <victoryohanna@gmail.com>
2023-09-27 14:29:44 +0100

    modified index.php

--- index.php
+++ index.php
@@ -1,28 +1,25 @@
 <?php
 
 /**
- * Plugin Name:       DoWellLabs-license-compatibility
+ * Plugin Name:       Dowell-license-compatibility
  * Description:       The Open Source License Compatibility check API provides access to a wide range of legal information and resources. It allows developers to retrieve information about software licenses, legal documents, legal entities, check license compatibility, and more. The API aims to facilitate the integration of legal information into applications, websites, or other software systems.
  * Requires at least: 5.8
  * Requires PHP:      7.0
  * Version:           1.0.0
- * Author:            Dowell Labs
+ * Author:            uxlivinglab
  * License:           GPL-2.0-or-later
  * Author URI:        https://github.com/DoWellLabs/Opensource-License-Compatibility
  * License URI:       https://www.gnu.org/licenses/gpl-2.0.html
  * 
  */
 
-
-
 //Admin Settings page
 
 function dowell_menu()
 {
   add_menu_page(
-
     'Dowell Opensource',
-    'Dowell Options',
+    'Dowell License',
     'manage_options',
     'dowell',
     'dowell_admin_page',
@@ -39,25 +36,6 @@ function dowell_admin_page()
 
 //Render page content
 add_action('admin_menu', 'dowell_menu');
-/**---------------------------------------------------------------- */
-
-
-//Add shortcode function
-
-// function compatibility($content)
-// {
-
-//   ob_start();
-//   $content =   require_once plugin_dir_path(__FILE__) . 'templates/app.php';
-//   if (is_file($content)) {
-//     @include($content);
-//   }
-
-//   return ob_get_clean();
-// }
-
-// add_shortcode('dowell', 'compatibility');
-/**-------------------------------------------------------------------------- */
 
 /** Enqueue scripts and styles. */
 
@@ -78,13 +56,11 @@ function displayReactApp()
 <?php return ob_get_clean();
 }
 
-
 add_shortcode('dowell', 'displayReactApp');
 add_action('wp_enqueue_scripts', 'enq_react');
 
 function enq_react()
 {
-
   wp_register_script(
     'display-react',
     plugin_dir_url(__FILE__) . '/build/index.js',
